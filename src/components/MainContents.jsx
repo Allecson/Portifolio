c172acd83976282fe17ddd4dc884c08be6b671ad
alecson <alecson73@gmail.com>
2023-09-18 17:38:07 -0300

    projetos páginas

--- src/components/MainContents.jsx
+++ src/components/MainContents.jsx
@@ -1,19 +1,26 @@
-import '../styles/components/MainContents.sass'
-import Avatar from "../img/Alecson.jpeg"
-const MainContents = () =>{
-    return <div id='div-main'>
-            <img src={Avatar} id="alecson"></img>
-            <div id='div-textos'>
-                <h3 id='h3-main'>Sobre Min</h3>
-                <p id='p-main'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Itaque eius quas, fuga ipsa et incidunt te
-                mporibus libero rerum, enim, minima quasi corporis. Eius, nihil cupiditate
-                 soluta quaerat provident commodi sunt!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque eius quas,
-                 fuga ipsa et incidunt temporibus libero rerum, enim, minima 
-                quasi corporis. Eius, nihil cupiditate soluta quaerat provident commodi sunt!</p>
-                <a href='#'>Baixar Currículo ➞</a>
-            </div>
-           </div>
+import "../styles/components/MainContents.sass";
+import Avatar from "../img/Alecson.jpeg";
+const MainContents = () => {
+  return (
+    <div id="div-main">
+      <img src={Avatar} id="alecson"></img>
+      <div id="div-textos">
+        <h3 id="h3-main">Sobre Mim</h3>
+        <p id="p-main">
+          "Olá, meu nome é Alecson. Depois que conheci a área de programação em
+          um curso técnico em informática no Senac em 2018, decidi que seria a
+          área em que eu iria me especializar. No entanto, com a pandemia, a
+          parte mais importante do curso, que era programação, acabou sendo um
+          desafio para os reitores da instituição. Então, essa parte do curso
+          não foi muito objetiva, e acabamos passando rapidamente por ela. Desde
+          então, venho estudando por conta própria a área e me aperfeiçoando.
+          Acredito que o próximo passo seja entrar em uma empresa profissional e
+          assim me especializar ao mesmo tempo em que ajudo a empresa. Acredito
+          que aprendo rápido e adoraria receber uma oportunidade."
+        </p>
+        <a href="https://github.com/Allecson">Meu GitHub ➞</a>
+      </div>
+    </div>
+  );
 };
 export default MainContents;
--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -1,8 +1,92 @@
-import '../styles/components/projetos.sass'
+import "../styles/components/projetos.sass";
+import { BiLinkExternal } from "react-icons/bi";
+import Dev1 from "../img/dev1.jpeg";
+import Dev2 from "../img/dev2.jpeg";
+import Dev3 from "../img/dev3.jpeg";
+import Dev4 from "../img/dev4.jpeg";
+import Dev5 from "../img/dev5.jpeg";
+import Dev6 from "../img/dev6.jpeg";
 
-const Projetos = () =>{
-    return <div>
-        <img class='imgs' src=''/>
-    </div>
-}
-export default Projetos
+
+const Projetos = () => {
+  return (
+    <section className="projects">
+      <h1 className="h1-projects">Minha Caminhada</h1>
+      <div className="div-projects">
+        <div className="div-img-texts-projects">
+          <img className="imgs" src={Dev1} />
+          <div className="texts-project">
+            <h2 className="h2-texts-project">Erro 404</h2>
+            <p className="p-texts-project">
+              Uma página que mostra um erro padrão de 404com uso de HTML,CSS<br></br><span>(Veja completo abaixo)</span>
+            </p>
+            <a href="https://allecson.github.io/404-not-found/" className="a-texts-project" target="_blank">
+              <BiLinkExternal />
+            </a>
+          </div>
+        </div>
+        <div className="div-img-texts-projects">
+          <img className="imgs" src={Dev2} />
+          <div className="texts-project">
+            <h2 className="h2-texts-project">Página Profissional</h2>
+            <p className="p-texts-project">
+              Uma página que mostra as habilidades pessoais de uma profissional com uso de HTML,CSS e JavaScript<br></br><span>(Veja completo abaixo)</span>
+            </p>
+            <a href="https://allecson.github.io/interior-consultant-master/" target="_blank" className="a-texts-project">
+              <BiLinkExternal />
+            </a>
+          </div>
+        </div>
+        <div className="div-img-texts-projects">
+          <img className="imgs" src={Dev3} />
+          <div className="texts-project">
+            <h2 className="h2-texts-project">Receita de Bolo</h2>
+            <p className="p-texts-project">
+              Uma página de uma receita de bolo com uso mais aprofundado de HTML, mas tambem utiliza CSS<br></br><span>(Veja completo abaixo)</span>
+            </p>
+            <a href="https://allecson.github.io/4-Recipe-page/" target="_blank" className="a-texts-project">
+              <BiLinkExternal />
+            </a>
+          </div>
+        </div>
+        <div className="div-img-texts-projects">
+          <img className="imgs" src={Dev4} />
+          <div className="texts-project">
+            <h2 className="h2-texts-project">Shopping</h2>
+            <p className="p-texts-project">
+            Uma Página de compras que utiliza HTML, CSS e JavaScript<br></br><span>(Veja completo abaixo)</span>
+            </p>
+            <a href="https://allecson.github.io/6-checkout-page-master/" className="a-texts-project" target="_blank">
+              <BiLinkExternal />
+            </a>
+          </div>
+        </div>
+        <div className="div-img-texts-projects">
+          <img className="imgs" src={Dev5} />
+          <div className="texts-project">
+            <h2 className="h2-texts-project">Portfólio</h2>
+            <p className="p-texts-project">
+              Um Portfólio de Edie, com uso melhorado de HTML,CSS e JavaScript<br></br><span>(Veja completo abaixo)</span>
+            </p>
+            <a href="https://allecson.github.io/7-edie-homepage-master/" className="a-texts-project" target="_blank">
+              <BiLinkExternal />
+            </a>
+          </div>
+        </div>
+        <div className="div-img-texts-projects">
+          <img className="imgs" src={Dev6} />
+          <div className="texts-project">
+            <h2 className="h2-texts-project">Galeria</h2>
+            <p className="p-texts-project">
+              Uma página de galeria com uso de HTML, SASS e JavaScript<br></br><span>(Veja completo abaixo)</span>
+            </p>
+            <a href='https://allecson.github.io/Projeto-SASS-galeria/' className="a-texts-project" target="_blank">
+              <BiLinkExternal />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Projetos;
